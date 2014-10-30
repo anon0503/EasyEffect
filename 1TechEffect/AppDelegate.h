cef0c488acc398bd29022ab74ff436abc5bec0f8
あのん の MacBook Pro <uchidakazuki@MacBook-Pro.local>
2014-10-30 20:46:08 +0900

    Initial Commit

---
+++ 1TechEffect/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  1TechEffect
+//
+//  Created by あのん の MacBook Pro on 2014/10/30.
+//  Copyright (c) 2014年 内田和希. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 1TechEffect/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  1TechEffect
+//
+//  Created by あのん の MacBook Pro on 2014/10/30.
+//  Copyright (c) 2014年 内田和希. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
